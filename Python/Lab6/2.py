921fcebbf0e1f98550ca67e67997ce291e7822f2
Diya <diyamaru32@gmail.com>
2026-06-12 17:27:05 +0530

    Create 2.py

---
+++ Python/Lab6/2.py
@@ -0,0 +1,12 @@
+n=int(input("Enter size:"))
+lst=[]
+for i in range(n):
+    lst.append(int(input("Enter element:")))
+print(lst)
+lst2=[]
+tup=tuple(lst)
+for i in tup:
+    if i not in lst2:
+        lst2.append(i)
+tup2=tuple(lst2)
+print("Updated tuple is:", tup2)
