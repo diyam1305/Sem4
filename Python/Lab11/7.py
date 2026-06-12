3efba3cf30966277277c33dd15c6518ab206b3d9
Diya <diyamaru32@gmail.com>
2026-06-12 17:43:09 +0530

    Create 7.py

---
+++ Python/Lab11/7.py
@@ -0,0 +1,9 @@
+import math as m
+r=int(input("Enter radius:"))
+b=int(input("Enter base:"))
+h=int(input("Enter height:"))
+l=int(input("Enter length:"))
+br=int(input("Enter breadth:"))
+print("Area of circle:", m.pi*r*r)
+print("Area of triangle:", 0.5*b*h)
+print("Area of rectangle:", l*br)
